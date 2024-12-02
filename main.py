4a0aa8b4977fa4844faba01be32b57b081ebb4db
arsrus721 <100342138+arsrus721@users.noreply.github.com>
2024-12-02 12:26:02 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-import gui
-import api_handler
+import tkinter as tk
+from gui import MinecraftLauncherApp
+from downloading import download_minecraft_version
+from api import get_minecraft_versions
+
+def main():
+    versions = get_minecraft_versions()
+
+    root = tk.Tk()
+    app = MinecraftLauncherApp(root, download_minecraft_version, get_minecraft_versions)
+    root.mainloop()
+
+if name == "main":
+    main()
