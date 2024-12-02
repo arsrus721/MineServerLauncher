3b9312f4a6d58f8cd05838c3523000a475e82ece
arsrus721 <100342138+arsrus721@users.noreply.github.com>
2024-12-02 12:36:18 +0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -11,6 +11,7 @@ root.geometry("700x500")
 btn = ttk.Button(text="Главное", command=main_button)
 btn = ttk.Button(text="Настройки", command=setinks_button)
 
+versions = versions_callback()
 combobox = ttk.Combobox(root, values=versions)
 combobox.pack(anchor=NW, padx=6, pady=6)
 
