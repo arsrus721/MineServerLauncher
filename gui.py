5dbf0250a3f2f957acc09beb0f93fc4852238b1b
liyaFPV <lbkmzc942@gmail.com>
2024-12-02 12:47:54 +0500

    gui

--- gui.py
+++ gui.py
@@ -8,10 +8,11 @@ root.title("MineServerLauncher")
 root.geometry("700x500") 
 
 btn = ttk.Button(text="Главное", command=main_button)
-btn = ttk.Button(text="Настройки", command=setinks_button)
+btn1 = ttk.Button(text="Настройки", command=setinks_button)
 
 combobox = ttk.Combobox(root, values=versions)
 combobox.pack(anchor=NW, padx=6, pady=6)
 
 btn.pack()
+btn1.pack()
 root.mainloop()
