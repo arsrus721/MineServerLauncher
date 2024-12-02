35de9dcef1ea24e1b89467d8ffe4629bf9dbf71c
arsrus721 <100342138+arsrus721@users.noreply.github.com>
2024-12-02 12:30:23 +0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -2,13 +2,17 @@ from tkinter import *
 from tkinter import ttk
 from api_handler import *
 from gui_comand import *
+
+
 root = Tk()
 root.title("MineServerLauncher")
 root.geometry("700x500") 
+
 btn = ttk.Button(text="Главное", command=main_button)
 btn = ttk.Button(text="Настройки", command=setinks_button)
+
 combobox = ttk.Combobox(root, values=versions)
 combobox.pack(anchor=NW, padx=6, pady=6)
-btn.pack()
 
+btn.pack()
 root.mainloop()
