085d33feb824288841ba6411b963ff6c08909c6e
liyaFPV <lbkmzc942@gmail.com>
2024-12-02 12:03:30 +0500

    gui

--- gui.py
+++ gui.py
@@ -1,6 +1,9 @@
 from tkinter import *
- 
+import gui_comand
 root = Tk()
 root.title("MineServerLauncher")
 root.geometry("700x500") 
+btn = ttk.Button(text="Click Me", command=main_button)
+btn.pack()
+
 root.mainloop()
---
+++ gui_comand.py
@@ -0,0 +1,2 @@
+def main_button():
+    print("1")
