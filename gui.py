83b50d5079512532e044dc79ffff3dc711d59e9d
liyaFPV <lbkmzc942@gmail.com>
2024-12-02 13:13:20 +0500

    gui

--- gui.py
+++ gui.py
@@ -19,7 +19,7 @@ btn2 = ttk.Button(text="Обновить", command=up_button)
 combobox = ttk.Combobox(root, values=versions)
 combobox.pack(anchor=NW, padx=6, pady=6)
 
-btn.pack()
+btn.place(x=20, y=30)
 btn1.pack()
 btn2.pack()
 root.mainloop()
